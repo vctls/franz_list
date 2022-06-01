d4bfb211b0c7f3cb36d600c28b1dfaabc8ac8fca
vctls <toulousevictor@gmail.com>
2022-06-01 18:41:53 +0200

    Adds some useless styling because why not.

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,17 @@ import "./App.css";
 import List from "./component/List";
 
 function App() {
+
+  const headerBackground: string = Math.floor(Math.random()*16777215).toString(16);
+  const headerColor: string = Math.floor(Math.random()*16777215).toString(16);
+  const style = {
+    backgroundColor: "#" + headerBackground,
+    color: "#" + headerColor
+  }
+  
   return (
     <div className="App">
-      <header className="App-header">
+      <header className="App-header" style={style}>
         <h1>Franz List</h1>
       </header>
       <List></List>
