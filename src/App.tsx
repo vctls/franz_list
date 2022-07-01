d981741b709a15fa794f3172d52aa4c97079edcc
Victor Toulouse <toulousevictor@gmail.com>
2022-07-01 19:35:16 +0200

    Adds GitHub link in footer for convenience.

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,30 @@
 import React from "react";
 import "./App.css";
 import List from "./component/List";
-import "@fontsource/water-brush"
+import "@fontsource/water-brush";
 
 function App() {
-
-  const headerBackground: string = Math.floor(Math.random()*16777215).toString(16);
-  const headerColor: string = Math.floor(Math.random()*16777215).toString(16);
+  const headerBackground: string = Math.floor(
+    Math.random() * 16777215
+  ).toString(16);
+  const headerColor: string = Math.floor(Math.random() * 16777215).toString(16);
   const style = {
     backgroundColor: "#" + headerBackground,
-    color: "#" + headerColor
-  }
-  
+    color: "#" + headerColor,
+  };
+
+  const logo = process.env.PUBLIC_URL + "/github_logo.svg";
+  const github_url = "https://github.com/vctls/franz_list/";
   return (
     <div className="App">
       <header className="App-header" style={style}>
         <h1>Franz List</h1>
       </header>
       <List></List>
+      <footer>
+        <img src={logo} alt="Github logo" height="16px"></img>{" "}
+        <a href={github_url}>{github_url}</a>
+      </footer>
     </div>
   );
 }
